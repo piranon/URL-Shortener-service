b0a69bd95ac51e19769080e86a3f21b371e34c5f
piranon <piranon.n@gmail.com>
2018-07-30 17:03:28 +0700

    Move user to Models dir, install and config data user seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+        $userQuantity = 1;
+        factory(User::class, $userQuantity)->create();
+    }
+}
